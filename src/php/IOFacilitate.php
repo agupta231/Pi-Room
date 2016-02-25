b28228d0ee22c88f8072b67819b9cc958ea2561b
Ankur Gupta <rukna1000@gmail.com>
2016-02-25 07:35:23 -0500

    Added files to make the server be able to use the data from the client and control the GPIO pins from that

---
+++ src/php/IOFacilitate.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * User: agupta
+ * Date: 2/25/16
+ */
+class IOFacilitate {
+    public function __construct() {
+
+    }
+}
---
+++ src/php/LED.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * User: agupta
+ * Date: 2/25/16
+ */
+class LED {
+
+}
